7e6967eee91a4e0c5a482470da0a7208b0d9568f
georvic <alexanderstower@gmail.com>
2018-05-06 21:19:15 -0400

    active and inactive users can be found out using websockets

--- active_users/consumers.py
+++ active_users/consumers.py
@@ -1,32 +1,55 @@
 # active_users/consumers.py
 
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 class ActiveUserConsumer(WebsocketConsumer):
 
     def connect(self):
         
-        print(self.scope)
-        #self.username = self.scope['url_route']['kwargs']['username']
+        remaining_path_elems = self.scope['path_remaining'].split('/')
         
-        """
+        self.username = remaining_path_elems[1]
+        
+        
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            "active"
+            "active",
+            self.channel_name
         )
-        """
-        self.accept()
         
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            "active",
+            {
+                'type': 'receive',
+                'message': self.username,
+                'status' : "active"
+            }
+        )
+        
+        self.accept()
+    
+    def status(self, arg) :
+        print(arg)
+    
     def disconnect(self, close_code):
-        pass
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            "active",
+            {
+                'type': 'receive',
+                'message': self.username,
+                'status' : "inactive"
+            }
+        )
         
     def receive(self, text_data):
         
-        text_data_json = json.loads(text_data)
-        
+        text_data_json = text_data
         message = text_data_json['message']
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'status' : text_data_json['status']
         }))
         
