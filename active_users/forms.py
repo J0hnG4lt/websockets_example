5be021566aec465df497243077a0c40e52f0b8d8
georvic <alexanderstower@gmail.com>
2018-05-07 09:22:20 -0400

    css style added

--- active_users/forms.py
+++ active_users/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UserForm(forms.Form):
-    username = forms.CharField(label='New User', max_length=200)
+    username = forms.CharField(label='Username', max_length=200)
--- websockets_example/settings.py
+++ websockets_example/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+
 redis_host = os.environ.get('REDIS_HOST', 'localhost')
 
 # Channel layer definitions
